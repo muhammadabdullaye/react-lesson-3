a1bb6fc58565244e1a5cbaeaf8da2a2245a8f3f2
muhammad <muhammad_09>
2026-02-28 18:11:52 +0500

    yana-ishlandi

--- src/componets/Shop.jsx
+++ src/componets/Shop.jsx
@@ -1,27 +1,112 @@
-const data = [{brand: 'Samsung', model: 'S24', price: 550,},{ brand: 'Samsung',model: 'S26 Ultra',price: 1890,},
-    {brand: 'Samsung', model: 'S15', price: 550,},{ brand: 'Samsung',model: 'S20 Ultra',price: 1890,},
-    {brand: 'Samsung', model: 'S16', price: 550,},{ brand: 'Samsung',model: 'S21 Ultra',price: 1890,},
-    {brand: 'Samsung', model: 'S17', price: 550,},{ brand: 'Samsung',model: 'S22 Ultra',price: 1890,},
-    {brand: 'Samsung', model: 'S18', price: 550,},{ brand: 'Samsung',model: 'S23 Ultra',price: 1890,}
+const data = [
+    {
+        brand: 'Samsung',
+        model: 'S24',
+        price: 550,
+        image: "https://macbro.uz/cdn/shop/files/9_21fc35b8-3291-4317-bdf5-19bce1ae9f2a.png?v=1747881900"
+    },
+    {
+        brand: 'Samsung',
+        model: 'S26 Ultra',
+        price: 1890,
+        image: "https://macbro.uz/cdn/shop/files/9_21fc35b8-3291-4317-bdf5-19bce1ae9f2a.png?v=1747881900"
+    },
+    {
+        brand: 'iPhone',
+        model: '13 Pro',
+        price: 700,
+        image: "https://macbro.uz/cdn/shop/files/9_21fc35b8-3291-4317-bdf5-19bce1ae9f2a.png?v=1747881900"
+    },
+    {
+        brand: 'iPhone',
+        model: 'SE',
+        price: 380,
+        image: "https://macbro.uz/cdn/shop/files/9_21fc35b8-3291-4317-bdf5-19bce1ae9f2a.png?v=1747881900"
+    },
+    {
+        brand: 'iPhone',
+        model: '12',
+        price: 380,
+        image: "https://macbro.uz/cdn/shop/files/9_21fc35b8-3291-4317-bdf5-19bce1ae9f2a.png?v=1747881900"
+    },
+    {
+        brand: 'iPhone',
+        model: '14',
+        price: 380,
+        image: "https://macbro.uz/cdn/shop/files/9_21fc35b8-3291-4317-bdf5-19bce1ae9f2a.png?v=1747881900"
+    },
+    {
+        brand: 'Samsung',
+        model: 'S26 Ultra',
+        price: 1890,
+        image: "https://macbro.uz/cdn/shop/files/9_21fc35b8-3291-4317-bdf5-19bce1ae9f2a.png?v=1747881900"
+    },
+    {
+        brand: 'Samsung',
+        model: 'S22 Ultra',
+        price: 1890,
+        image: "https://macbro.uz/cdn/shop/files/9_21fc35b8-3291-4317-bdf5-19bce1ae9f2a.png?v=1747881900"
+    },
+    {
+        brand: 'Redmi',
+        model: 'Not 13',
+        price: 300,
+        image: "https://macbro.uz/cdn/shop/files/9_21fc35b8-3291-4317-bdf5-19bce1ae9f2a.png?v=1747881900"
+    },
+    {
+        brand: 'Redmi',
+        model: 'Not 14 pro',
+        price: 300,
+        image: "https://macbro.uz/cdn/shop/files/9_21fc35b8-3291-4317-bdf5-19bce1ae9f2a.png?v=1747881900"
+    },
+    {
+        brand: 'Redmi',
+        model: 'Not 10c',
+        price: 200,
+        image: "https://macbro.uz/cdn/shop/files/9_21fc35b8-3291-4317-bdf5-19bce1ae9f2a.png?v=1747881900"
+    },
 ]
 
-
-function Shop() {
+function Shop () {
     return (
-       <>
-       <h1>Shop</h1>
-       <ul>
-        {
-            data.map( ({brand, model, price}) => {
-                       
-            return (
         <>
-        <li> {brand} {model}: {price}$ </li>
-        </>
-        )
-        } )
+<div className="container">
+    <h1 className="text-danger">Shop</h1>
+    <div className="card-container d-flex gap-3">
+        {
+            data.map( ({ brand, model, price, image }) => {
+                return (
+                    <>
+                        <div class="card" style={{ width: "20rem" }}>
+                        <img src={image} class="card-img-top" alt="rasm" />
+                        <div class="card-body">
+                            <h5 class="card-title"> {brand} </h5>
+                            <p class="card-text"> {model} </p>
+                            <p class="card-text"> ${price} </p>
+                            <a href="#" class="btn btn-primary">Buy</a>
+                        </div>
+                        </div>
+                    </>
+                )
+            } )
         }
-        </ul>
+    </div>
+</div>
+
+
+            {/* <ul>
+
+                {
+                    data.map( ({brand, model, price}) => {
+                       
+                        return (
+                            <>
+                                <li> {brand} {model}: {price}$ </li>
+                            </>
+                        )
+                    } )
+                }
+            </ul> */}
         </>
     )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,12 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router";
+import "bootstrap/dist/css/bootstrap.css"
+import "bootstrap/dist/js/bootstrap.js"
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
       <App />
     </BrowserRouter>
   </StrictMode>
-);
+);  
